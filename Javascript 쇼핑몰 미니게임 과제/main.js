5dac100be08d6581dd310fefb35caacce26038cd
jun3327 <spdlqjdkdlel1702@naver.com>
2023-01-04 19:47:59 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -7,9 +7,26 @@ function loadItems() {
 
 function displayItems(item) {
     const container = document.querySelector('.items');
-    container.innerHTML = items.map(item=>createHTMLString(item)).join('');
+    container.innerHTML = item.map(item=>createHTMLString(item)).join('');
 }
 
+function onButtonClick(event,items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return ;
+    }
+    displayItems(items.filter(items[key]===value))
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click',event=>onButtonClick(event,items));
+}
 function createHTMLString(item) {
     return `
     <li class="item">
