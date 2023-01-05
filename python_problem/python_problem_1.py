64407080238d7595028987f515a06e9d0dda4663
jun3327 <spdlqjdkdlel1702@naver.com>
2023-01-06 02:42:16 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,9 +1,10 @@
 num = 0
-while(True):
+def brGame():
+    call = 0
     while(True):
         try:
-            callA = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if ( not 0<callA<4 ):
+            call = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if ( not 0<call<4 ):
                 raise Exception("")
         except ValueError:
             print("정수를 입력하세요")
@@ -11,33 +12,27 @@ while(True):
             print("1,2,3 중 하나를 입력하세요")
         else:
             break;
+    return call
 
-    for i in range(callA):
-        print("playerA: {}".format(i+num+1))
-    num = num + callA
+while(True):
+
+    add1 = brGame()
+    for i in range(add1):
+        print("playerA: {}".format(i+1+num))
+
+    num = num + add1
     if(num>30):
         print("playerA win!") 
         break;
 
-    while(True):
-        try:
-            callB = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if ( not 0<callB<4 ):
-                raise Exception("")
-        except ValueError:
-            print("정수를 입력하세요")
-        except Exception:
-            print("1,2,3 중 하나를 입력하세요")
-        else:
-            break;
+    add2 = brGame()
+    for i in range(add2):
+        print("playerB: {}".format(i+1+num))
 
-    for i in range(callB):
-        print("playerB: {}".format(i+num+1))
-    num = num + callB
+    num = num + add2
     if(num>30):
         print("playerB win!") 
         break;
-    
 
 
 
