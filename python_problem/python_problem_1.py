304e0b70f4f9096945df514fc0561b935fc2b1a7
jun3327 <spdlqjdkdlel1702@naver.com>
2023-01-06 01:15:41 +0900

    1-6(수정)

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,7 +1,4 @@
 num = 0
-callA = 0
-callB = 0
-count = 0
 while(True):
     while(True):
         try:
@@ -15,14 +12,12 @@ while(True):
         else:
             break;
 
-    for num in range(callA):
-        print("playerA: {}".format(num+1+count))
-    count = count + callA
-    
-    if(count>30):
+    for i in range(callA):
+        print("playerA: {}".format(i+num+1))
+    num = num + callA
+    if(num>30):
         break;
 
-    num = 0
     while(True):
         try:
             callB = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
@@ -35,13 +30,13 @@ while(True):
         else:
             break;
 
-    for num in range(callB):
-        print("playerB: {}".format(num+1+count))
-    count = count + callB
-
-    if(count>30):
-        
+    for i in range(callB):
+        print("playerB: {}".format(i+num+1))
+    num = num + callB
+    if(num>30):
         break;
+    
+
 
 
     
