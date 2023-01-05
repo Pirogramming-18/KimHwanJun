278f09ff933024e3f14cadea6ccbe7361b95ef13
jun3327 <spdlqjdkdlel1702@naver.com>
2023-01-06 02:56:27 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,5 +1,22 @@
+import random
+
 num = 0
 def brGame():
+    call = 0
+    while(True):
+        try:
+            call =random.randint(1,3)
+            if ( not 0<call<4 ):
+                raise Exception("")
+        except ValueError:
+            print("정수를 입력하세요")
+        except Exception:
+            print("1,2,3 중 하나를 입력하세요")
+        else:
+            break;
+    return call
+
+def brGame2():
     call = 0
     while(True):
         try:
@@ -18,20 +35,20 @@ while(True):
 
     add1 = brGame()
     for i in range(add1):
-        print("playerA: {}".format(i+1+num))
+        print("Computer: {}".format(i+1+num))
 
     num = num + add1
     if(num>30):
-        print("playerA win!") 
+        print("Computer win!") 
         break;
 
-    add2 = brGame()
+    add2 = brGame2()
     for i in range(add2):
-        print("playerB: {}".format(i+1+num))
+        print("player: {}".format(i+1+num))
 
     num = num + add2
     if(num>30):
-        print("playerB win!") 
+        print("player win!") 
         break;
 
 
